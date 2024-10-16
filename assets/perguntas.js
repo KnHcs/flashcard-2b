9e3aadd60a75819bd673f8ae738be6bf2cc26321
KnHcs <163007643+KnHcs@users.noreply.github.com>
2024-10-16 12:14:31 -0300

    .

---
+++ assets/perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Programação 2.0',
+    'O que é Flamengo',
+    'Para o cego, a luz. para o mudo, a voz. para o burro, a inteligência.'
+)
+
+criaCartao(
+    'Vasco',
+    'Quem fundo o Vasco?',
+    'Mateus Ferreira Biazzeto'
+)
+
+criaCartao(
+    'Batata',
+    'Quem plantou batata em contenda?',
+    'Mateus Ferreira Biazzeto'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem é conhecido por ser pai do Vasco?',
+    'O Flamengo'
+)
+
+criaCartao(
+    'História',
+    'Quem descobriu o Brasil',
+    'Pedrinho matador'
+)
+
+criaCartao(
+    'Futebol',
+    'Quem construiu o estádio do Flamengo',
+    'Conserta Felix'
+)
