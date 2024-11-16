0813c9af200e99bee33e37decccd739ec7b1e179
KnHcs <163007643+KnHcs@users.noreply.github.com>
2024-11-16 09:39:01 -0300

    Update pergunta.js

--- pergunta.js
+++ pergunta.js
@@ -1,35 +1,35 @@
 criaCartao(
-    'Programação 2.0',
+    'Phyton',
     'O que é phyton?',
-    'O phyton é uma linguagem de progamação'
+    'O phyton é uma linguagem de programação'
 )
-
 criaCartao(
-    'Vasco',
-    'Quem fundo o Vasco?',
-    'Mateus Ferreira Biazzeto',
+    'Site',
+    'Html é usado para que?',
+    'Criação de Sites'
 )
-
 criaCartao(
-    'Batata',
-    'Quem plantou batata em contenda?',
-    'Mateus Ferreira Biazzeto'
+    'PHP',
+    'O que é PHP?',
+    'O PHP é uma linguagem de programação'
 )
-
 criaCartao(
-    'Futebol',
-    'Quem é conhecido por ser pai do Flamengo?',
-    'O vasco'
+    'C#',
+    'C# é uma linguagem de programação, multiparadigma,?',
+    'O phyton é uma linguagem de programação'
 )
-
 criaCartao(
-    'História',
-    'Quem descobriu o Brasil?',
-    'Pedrinho matador'
+    'multiparadigma',
+    'O que é multiparadigma?',
+    'a linguagem multiparadigma nada mais é a programação que suporta vários paradigmas de programação'
 )
-
 criaCartao(
-    'Futebol',
-    'Quem construiu o estádio do Vasco?',
-    'Conserta Felix'
+    'C++',
+    'O que é C++?',
+    'é uma linguagem de programação compilada multi-paradigma e de uso geral'
+)
+criaCartao(
+    'Assembly',
+    'O que é linguagem assembly?',
+    ' é uma linguagem de programação de baixo nível'
 )
